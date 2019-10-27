ad6c3f709abe24f0889f53e1c91cd5ee9370ae6c
CJHydraGenZ <cahyasaga321@gmail.com>
2019-10-27 12:25:02 +0800

    test logika

---
+++ export/main.js
@@ -0,0 +1,14 @@
+const data = [{
+        buleleng: 1000,
+        kelungkung: 2000,
+        karangasem: 1200,
+        denpasar: 5000,
+        negara: 2000,
+        gianyar: 1000,
+        tabanan: 4000
+    },
+    {
+        pendidikan: 10000,
+        namaInstansi: "Cahya"
+    }
+]
---
+++ test logika/main.js
@@ -0,0 +1,117 @@
+let string = ''
+for (let i = 0; i < 10; i++) {
+
+    for (let j = 0; j <= i; j++) {
+        for (let k = j; k < i; k++) {
+
+            string += '*'
+        }
+
+
+    }
+
+
+
+
+
+
+
+    string += '\n';
+
+
+}
+
+for (let x = 10; x > 0; x--) {
+
+    for (let y = 0; y <= x; y++) {
+        for (let z = y; z < x; z++) {
+            string += '*'
+
+        }
+
+
+    }
+
+    string += '\n';
+
+
+}
+for (let i = 0; i < 10; i++) {
+
+    for (let j = 0; j <= i; j++) {
+        for (let k = j; k < i; k++) {
+
+            string += '*'
+        }
+
+
+    }
+
+
+
+
+
+
+
+    string += '\n';
+
+
+}
+
+for (let x = 10; x > 0; x--) {
+
+    for (let y = 0; y <= x; y++) {
+        for (let z = y; z < x; z++) {
+            string += '*'
+
+        }
+
+
+    }
+
+    string += '\n';
+
+
+}
+
+
+
+
+
+
+
+
+
+console.log(string);
+
+// document.write("- Segitiga 3<br>");
+// for (var i = 1; i <= 5; i++) {
+//     for (var j = i; j <= 5; j++) {
+//         document.write("_");
+//     }
+//     for (var k = 1; k <= i; k++) {
+//         document.write("*");
+//     }
+//     document.write("<br>");
+// }
+// document.write("- Segitiga 4<br>");
+// for (var i = 1; i <= 5; i++) {
+//     for (var j = 1; j <= i; j++) {
+//         document.write("_");
+//     }
+//     for (var k = i; k <= 5; k++) {
+//         document.write("*");
+//     }
+//     document.write("<br>");
+// }
+
+// document.write("- Segitiga 3<br>");
+// for (var i = 1; i <= 5; i++) {
+//     for (var j = i; j <= 5; j++) {
+//         document.write("_");
+//     }
+//     for (var k = 1; k <= i; k++) {
+//         document.write("*");
+//     }
+//     document.write("<br>");
+// }
